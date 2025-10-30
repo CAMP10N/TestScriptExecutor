89f96ad2ea16b53a3108c4ef21f7e4bad87313cb
Giorgi Chalagashvili <giorgi.chalagashvili@azry.com>
2025-10-30 17:27:34 +0400

    giviko

---
+++ scripts/givi.lua
@@ -0,0 +1,11 @@
+local function helper(arg1, arg2, arg3)
+	return { arg1, arg2, arg3 }
+end
+
+function testFunc(arg1, arg2, arg3)
+	return helper(arg1, arg2, arg3)
+end
+
+function getScriptSignature()
+	return { arg1 = "number", arg2 = "string", arg3 = "number" }
+end
