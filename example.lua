c0ab70e7133eb62fbb1ed74e6556e73cd9e19e27
Giorgi Chalagashvili <giorgi.chalagashvili@azry.com>
2025-10-30 17:20:01 +0400

    add complex script

--- example.lua
+++ example.lua
@@ -1,3 +1,12 @@
-function testFunc()
-    return { message = "Hello from Git!" }
-end 
+local function helper(arg1, arg2, arg3)
+	return { arg1, arg2, arg3 }
+end
+
+function testFunc(arg1, arg2, arg3)
+	return helper(arg1, arg2, arg3)
+end
+
+function getSignature()
+	return { arg1 = "number", arg2 = "string", arg3 = "number" }
+end
+
