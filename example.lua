3e102622df8d1c930be0245a8145c89c5865fc2b
Giorgi Chalagashvili <giorgi.chalagashvili@azry.com>
2025-10-30 17:23:36 +0400

    change function name

--- example.lua
+++ example.lua
@@ -6,7 +6,7 @@ function testFunc(arg1, arg2, arg3)
 	return helper(arg1, arg2, arg3)
 end
 
-function getSignature()
+function getScriptSignature()
 	return { arg1 = "number", arg2 = "string", arg3 = "number" }
 end
 
